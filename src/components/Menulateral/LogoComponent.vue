f8411201142e9c2cf140f14ac6c0c3080aa217a1
RafaelEsp <rafael.espinoza.1993@gmail.com>
2021-08-12 04:06:30 -0400

    ...

--- src/components/Menulateral/LogoComponent.vue
+++ src/components/Menulateral/LogoComponent.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     #LogoComponent
         figure.image.is-128x128
-            img.logo(src="http://i.pinimg.com/originals/5e/46/8f/5e468fcd23da1191459473bc9c90c31e.png")
+            img.logo(src="https://i.pinimg.com/originals/5e/46/8f/5e468fcd23da1191459473bc9c90c31e.png")
 </template>
 <script>
 export default {
